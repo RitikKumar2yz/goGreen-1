6fa6f07aa6da78635a270f3fb07ea802f36b438f
Ritik Kumar <ritikkumarparsa@gmail.com>
2025-07-16 21:52:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,36 +5,31 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
-};
+// 🔁 New date range: 15 Jan 2018 → 1 July 2025
+const START_DATE = moment("2018-01-8");
+const END_DATE = moment("2025-07-01");
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
+  const x = random.int(0, 388); // ~388 weeks total
+  const y = random.int(0, 6);   // 0 to 6 days
+
+  const date = START_DATE.clone().add(x, "w").add(y, "d");
+
+  if (date.isAfter(END_DATE)) return makeCommits(n);  
+
+  const formattedDate = date.format();
+  console.log(formattedDate);
+
+  const data = { date: formattedDate };
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit()
+      .add([path])
+      .commit(formattedDate, { "--date": formattedDate }, makeCommits.bind(this, --n));
   });
 };
 
-makeCommits(100);
+// 🟢 Number of commits (increase to fill contribution graph)
+makeCommits(1000);
